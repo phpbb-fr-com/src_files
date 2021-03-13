50df3ef84321e42e1b80909285c7dabe6b257c13
Mario Skouat <forum@skouat.org>
2021-03-13 02:16:34 +0100

    Fix news.php - base version

--- phpbb_sessions/news/base/news.php
+++ phpbb_sessions/news/base/news.php
@@ -7,10 +7,12 @@ $user->session_begin();
 $auth->acl($user->data);
 $user->setup();
 
-$forum_id = $request->variable('forum_id', 0);
-$where = ($forum_id) ? ' WHERE forum_id= ' . (int) $forum_id : ' WHERE' . $db->sql_in_set('forum_id', array_keys($auth->acl_getf('f_read', true)));
+$user_loader = $phpbb_container->get('user_loader');
 
-$sql = 'SELECT forum_id,topic_id, topic_time, topic_title, topic_views, topic_replies, topic_poster, topic_first_poster_name, topic_first_poster_colour, topic_last_post_id, topic_last_poster_id, topic_last_poster_name, topic_last_poster_colour, topic_last_post_time
+$forum_id = $request->variable('forum_id', 0);
+
+$where = ($forum_id) ? ' WHERE forum_id= ' . (int) $forum_id : ' WHERE ' . $db->sql_in_set('forum_id', array_keys($auth->acl_getf('f_read', true)));
+$sql = 'SELECT forum_id, topic_id, topic_time, topic_title, topic_views, topic_posts_approved, topic_poster, topic_last_post_id, topic_last_poster_id, topic_last_post_time
 	FROM ' . TOPICS_TABLE .
 	$where . '
 		AND topic_status <> ' . ITEM_MOVED . ' 
@@ -30,7 +32,7 @@ $result = $db->sql_query_limit($sql, 10);
 	</style>
 </head>
 <body>
-<table width="100%">
+<table style="width: 100%">
 	<tr>
 		<th>Sujets</th>
 		<th>Réponses</th>
@@ -48,13 +50,13 @@ $result = $db->sql_query_limit($sql, 10);
 		echo '<tr bgcolor="' . $row_colour .'">';
 		echo '<td>';
 		echo '<a href="' . $view_topic_url . '">' . censor_text($row['topic_title']) . '</a><br>';
-		echo $language->lang('POST_BY_AUTHOR') . '&nbsp;' . get_username_string('full', $row['topic_poster'], $row['topic_first_poster_name'], $row['topic_first_poster_colour']);
+		echo $language->lang('POST_BY_AUTHOR') . '&nbsp;' . $user_loader->get_username($row['topic_poster'], 'full', false, false, true);
 		echo '&nbsp;' . $language->lang('POSTED_ON_DATE') . '&nbsp;' . $user->format_date($row['topic_time']) . '</td>';
-		echo '<td>' . $row['topic_replies'] . '</td>';
+		echo '<td>' . ($row['topic_posts_approved'] - 1) . '</td>';
 		echo '<td>' . $row['topic_views'] . '</td>';
-		echo '<td>' . $language->lang('POST_BY_AUTHOR') . '&nbsp;' . get_username_string('full', $row['topic_last_poster_id'], $row['topic_last_poster_name'], $row['topic_last_poster_colour']);
-		echo '<a href="' . $view_topic_url . '&amp;p=' . $row['topic_last_post_id'] . '#p' . $row['topic_last_post_id'] . '">' . '&nbsp;' . $user->img('icon_topic_latest', 'VIEW_LATEST_POST') . '</a><br>';
-		echo $language->lang('POSTED_ON_DATE') . $user->format_date($row['topic_last_post_time']) . '</td>';
+		echo '<td>' . $language->lang('POST_BY_AUTHOR') . '&nbsp;' . $user_loader->get_username($row['topic_last_poster_id'], 'full', false, false, true);
+		echo '&nbsp;<a href="' . $view_topic_url . '&amp;p=' . $row['topic_last_post_id'] . '#p' . $row['topic_last_post_id'] . '">' . $user->img('icon_topic_latest', 'VIEW_LATEST_POST') . '</a><br>';
+		echo $language->lang('POSTED_ON_DATE') . '&nbsp;' .$user->format_date($row['topic_last_post_time']) . '</td>';
 		echo '</tr>';
 		$i++;
 	}
